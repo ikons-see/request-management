d270f47fac7f5a5cb907b00015b76828c8b64c90
sharkoandi <asharko@ikons-see.com>
2021-02-12 11:06:01 +0100

    quick fix on reports date format

--- backend/src/main/java/com/ikons/requestmanagement/core/usecase/reports/TotalRequestsPerMonthUseCase.java
+++ backend/src/main/java/com/ikons/requestmanagement/core/usecase/reports/TotalRequestsPerMonthUseCase.java
@@ -1,9 +1,12 @@
 package com.ikons.requestmanagement.core.usecase.reports;
 
 import com.ikons.requestmanagement.core.dto.MonthlyReportsDto;
+import com.ikons.requestmanagement.core.dto.ReportsResponseDto;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TotalRequestsPerMonthUseCase {
@@ -14,8 +17,17 @@ public class TotalRequestsPerMonthUseCase {
     this.reportResults = reportResults;
   }
 
-  public List<MonthlyReportsDto> totalRequestsPerMonth(){
+  public List<ReportsResponseDto> totalRequestsPerMonth() {
+
     final List<MonthlyReportsDto> requestsPerMonth = reportResults.requestsPerMonth();
-    return requestsPerMonth;
+    return requestsPerMonth.stream().map(period -> {
+      final Calendar calendar = Calendar.getInstance();
+      calendar.set(Calendar.YEAR, period.getYear());
+      calendar.set(Calendar.MONTH, period.getMonth());
+      return ReportsResponseDto.builder()
+          .period(calendar.getTime())
+          .total(period.getTotal())
+          .build();
+    }).collect(Collectors.toList());
   }
 }
--- backend/src/main/java/com/ikons/requestmanagement/core/usecase/reports/TotalResourcesPerMonthUseCase.java
+++ backend/src/main/java/com/ikons/requestmanagement/core/usecase/reports/TotalResourcesPerMonthUseCase.java
@@ -1,9 +1,12 @@
 package com.ikons.requestmanagement.core.usecase.reports;
 
 import com.ikons.requestmanagement.core.dto.MonthlyReportsDto;
+import com.ikons.requestmanagement.core.dto.ReportsResponseDto;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TotalResourcesPerMonthUseCase {
@@ -14,7 +17,17 @@ public class TotalResourcesPerMonthUseCase {
     this.reportResults = reportResults;
   }
 
-  public List<MonthlyReportsDto> totalResourcesPerMonth(){
-    return reportResults.resourcesPerMonth();
+  public List<ReportsResponseDto> totalResourcesPerMonth(){
+
+    final List<MonthlyReportsDto> resourcesPerMonth = reportResults.resourcesPerMonth();
+    return resourcesPerMonth.stream().map(period -> {
+      final Calendar calendar = Calendar.getInstance();
+      calendar.set(Calendar.YEAR, period.getYear());
+      calendar.set(Calendar.MONTH, period.getMonth());
+      return ReportsResponseDto.builder()
+          .period(calendar.getTime())
+          .total(period.getTotal())
+          .build();
+    }).collect(Collectors.toList());
   }
 }
